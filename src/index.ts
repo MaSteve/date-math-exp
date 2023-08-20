bd82360cee2065e3505447940a82f85e8c431569
Marco Antonio Garrido Rojo <marco@raincoatmoon.com>
2023-08-20 12:34:52 +0200

    Add some tests for parse function

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { tokenize } from './tokenizer';
 
 type DateString = string;
 
-export function parse(datestring: DateString): Date {
-  return parseDateMathExpression(tokenize(datestring)).evaluate()!;
+export function parse(datestring: DateString, dateOverride?: Date): Date {
+  return parseDateMathExpression(tokenize(datestring)).evaluate(dateOverride)!;
 }
 
 export function stringify(date: Date): DateString {
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,14 @@
 import { parse } from '../src/index';
 
-describe('test', () => {
-  it('should test', () => {
-    console.log('----->>>>', parse('now'));
-    console.log('----->>>>', parse('now+1y'));
-    console.log('----->>>>', parse('now+1w'));
-    console.log('----->>>>', parse('now/y'));
-    console.log('----->>>>', parse('now/M'));
-    console.log('----->>>>', parse('now/w'));
-    console.log('----->>>>', parse('now/d'));
-    console.log('----->>>>', parse('now/h'));
-    console.log('----->>>>', parse('now/m'));
-    console.log('----->>>>', parse('now/s'));
+describe('parse', () => {
+  it.each([
+    ['now-1y/y', '2020-05-01T00:00:00.000Z', '2019-01-01T00:00:00.000Z'],
+    ['now/y', '2020-05-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z'],
+    ['now-1d', '2020-05-01T00:00:00.000Z', '2020-04-30T00:00:00.000Z'],
+    ['now+1d', '2020-05-01T00:00:00.000Z', '2020-05-02T00:00:00.000Z'],
+    ['now-4d-4h', '2020-05-01T00:00:00.000Z', '2020-04-26T20:00:00.000Z'],
+  ])('should return expected date', (expression, dateOverride, expected) => {
+    let res = parse(expression, new Date(dateOverride));
+    expect(res).toEqual(new Date(expected));
   });
 });
